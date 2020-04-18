c6f13226f10a13166e30787f61ae4e6af891338a
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-04-18 19:57:50 +0200

    Added entry point

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Next Desk</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
