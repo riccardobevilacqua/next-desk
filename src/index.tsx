6c8d826265fd4a6a2b0d8182dacfa69cf4c4a004
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-04-18 22:19:48 +0200

    Updated layout

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,16 @@ import './styles/styles.scss';
 
 const App: React.FunctionComponent = () => {
   return (
-    <div>
-      <h1>Next Desk</h1>
-    </div>
+    <>
+      <section className="hero is-primary is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Next Desk</h1>
+            <h2 className="subtitle">Choose your next office to work from.</h2>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
