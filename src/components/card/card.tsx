38bb7b4f8b4f976dc5a61ab0ab120fbead480a21
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-04-19 18:34:41 +0200

    Added card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card: React.FunctionComponent = () => {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <p className="title">Card Title</p>
+        <p>Some Text</p>
+      </div>
+    </div>
+  );
+}
+
+export { Card };
