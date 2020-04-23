5bd1725cee1f1673cea99c2bab375734d9f19508
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-04-23 10:05:38 +0200

    Removed unnecessary code

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 
 import { Header } from './components/header/header';
 import { Planner } from './components/planner/planner';
-import { createMockserver } from './mock/mock-server';
 
 import './styles/styles.scss';
-
-createMockserver();
 
 const App: React.FunctionComponent = () => {
   return (
