4a2ece8b9d5704a384a6a7c3e62d7686f0960b58
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-04-24 00:52:38 +0200

    Updated webpack configuration

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,23 @@
 const merge = require('webpack-merge');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env)
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 });
